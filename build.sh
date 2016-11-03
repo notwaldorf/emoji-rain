72c812b93218c31d15ee1d4a3940b2422b106dd5
Monica Dinculescu <noms@google.com>
2016-11-03 16:49:02 -0700

    reorg repo to work on the webcomponents catalog

--- build.sh
+++ build.sh
@@ -1,20 +1,52 @@
-## note: you should have run bower install before running this.
+#!/bin/bash -e
+#
+# @license
+# Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+# Code distributed by Google as part of the polymer project is also
+# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+#
 
-org=${1:-"notwaldorf"}
-repo=${2:-"emoji-rain"}
+# This script pushes a demo-friendly version of your element and its
+# dependencies to gh-pages.
 
-polybuild index.html
-echo js:   `gzip -c index.build.js | wc -c`
-echo html: `gzip -c index.build.html | wc -c`
+# Run in a clean directory passing in a GitHub org and repo name
+org="notwaldorf"
+repo="emoji-rain"
+branch="master" # default to master when branch isn't specified
 
-rm -rf deploy
-git clone -b gh-pages --single-branch https://github.com/$org/$repo.git deploy
+# make folder (same as input, no checking!)
+mkdir $repo
+git clone https://github.com/$org/$repo.git --single-branch
 
-mv index.build.html deploy/index.html
-mv index.build.js deploy
+# switch to gh-pages branch
+pushd $repo >/dev/null
+git checkout --orphan gh-pages
 
-pushd deploy >/dev/null
+# remove all content
+git rm -rf -q .
+
+# use bower to install runtime deployment
+bower cache clean $repo # ensure we're getting the latest from the desired branch.
+git show ${branch}:bower.json > bower.json
+echo "{
+  \"directory\": \"components\"
+}
+" > .bowerrc
+bower install
+bower install $org/$repo#$branch
+git checkout ${branch} -- demo
+rm -rf components/$repo/demo
+mv demo components/$repo/
+
+# redirect by default to the component folder
+echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
+
+# send it all to github
 git add -A .
-git commit -am 'update gh-pages'
-git push -u origin gh-pages:gh-pages -f
+git commit -am 'seed gh-pages'
+git push -u origin gh-pages --force
+
 popd >/dev/null
