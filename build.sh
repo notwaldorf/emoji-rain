2155739f73b657896edad8cbcd6318b4d1276f37
Monica Dinculescu <noms@google.com>
2016-11-03 16:32:14 -0700

    update gh-pages

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo js:   `gzip -c index.build.js | wc -c`
 echo html: `gzip -c index.build.html | wc -c`
 
 rm -rf deploy
-git clone -b gh-pages --single-branch git@github.com:$org/$repo.git deploy
+git clone -b gh-pages --single-branch https://github.com/$org/$repo.git deploy
 
 mv index.build.html deploy/index.html
 mv index.build.js deploy
