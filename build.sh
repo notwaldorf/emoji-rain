5ffc28c79d1add1c123d9da83294070ff65aeb98
Monica Dinculescu <noms@google.com>
2016-11-03 16:31:43 -0700

    add gh-pages build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+org=${1:-"notwaldorf"}
+repo=${2:-"emoji-rain"}
+
+polybuild index.html
+echo js:   `gzip -c index.build.js | wc -c`
+echo html: `gzip -c index.build.html | wc -c`
+
+rm -rf deploy
+git clone -b gh-pages --single-branch git@github.com:$org/$repo.git deploy
+
+mv index.build.html deploy/index.html
+mv index.build.js deploy
+
+pushd deploy >/dev/null
+git add -A .
+git commit -am 'update gh-pages'
+git push -u origin gh-pages:gh-pages -f
+popd >/dev/null
